aeec97c1c384ca9991f26c8de0c6295d2a4fef5b
BearGom2 <31925642+sjcalaa@users.noreply.github.com>
2021-09-24 00:07:24 +0900

    로그인 기능 활성화

--- app/src/main/java/com/portfolio/makeitportfolio/hanlight_aos/Net/Clinet.kt
+++ app/src/main/java/com/portfolio/makeitportfolio/hanlight_aos/Net/Clinet.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Client {
-    private const val BASE_URL: String = "https://api.hanlight.kr/"
+    private const val BASE_URL: String = "http://chunwol.xyz:8000/"
     val getClient: API
         get() {
             val gson = GsonBuilder()
